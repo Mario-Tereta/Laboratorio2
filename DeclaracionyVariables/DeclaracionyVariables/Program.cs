fbce3d3800e49cb2f2330dbb0b96c6dd218f7fed
Mario-Tereta <mteretas@miumg.edu.gt>
2025-02-13 21:50:50 -0600

    Level 1

---
+++ DeclaracionyVariables/DeclaracionyVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DeclaracionyVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumEntero;
+            double NumDecimal;
+            string txt;
+            bool Bool;
+                     
+            Console.Write("Introduce un número entero: ");
+            NumEntero = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduce un número decimal: ");
+            NumDecimal = double.Parse(Console.ReadLine());
+
+            Console.Write("Introduce un texto: ");
+            txt = Console.ReadLine();
+
+            Console.Write("Introduce un valor verdadero o falso (true/false): ");
+            Bool = bool.Parse(Console.ReadLine());
+                       
+            Console.WriteLine($"Número entero: {NumEntero}");
+            Console.WriteLine($"Número decimal: {NumDecimal}");
+            Console.WriteLine($"Texto: {txt}");
+            Console.WriteLine($"Valor booleano: {Bool}");
+        }
+    }
+}
